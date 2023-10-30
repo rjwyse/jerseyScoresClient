26eb741fc580af1e09d86fb0c334b4767553177a
rjwyse <wyse.rob27@gmail.com>
2023-10-30 13:33:05 -0400

    lunch30

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -41,7 +41,7 @@ export default function NewOrderPage({ user, setUser }) {
   async function handleAddToOrder(itemId) {
   // alert(`add item: ${itemId}`);
   // 1. Call the addItemToCart function in ordersAPI, passing to it the itemId, and assign the resolved promise to a variable named cart.
-    const updatedCart = await addItemToCart(itemId);
+    const updatedCart = await ordersAPI.addItemToCart(itemId);
   // 2. Update the cart state with the updated cart received from the server
     setCart(updatedCart);
   }
@@ -53,8 +53,10 @@ export default function NewOrderPage({ user, setUser }) {
 
   async function handleCheckout() {
     await ordersAPI.checkout();
-    navigate('/orders');
+    const orderData = encodeURIComponent(JSON.stringify(cart));
+    navigate(`/orders?orderData=${orderData}`);
   }
+
 
   return (
     <main className="NewOrderPage">
@@ -72,7 +74,7 @@ export default function NewOrderPage({ user, setUser }) {
         jerseyItems={jerseyItems.filter(item => item.category.name === activeCat)}
         handleAddToOrder={handleAddToOrder}
       />
-      <OrderDetail order={cart}handleChangeQty={handleChangeQty}/>
+      <OrderDetail order={cart}handleChangeQty={handleChangeQty}handleCheckout={handleCheckout}/>
     </main>
   );
 }
--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,7 +1,28 @@
+import { useLocation } from 'react-router-dom';
+
 export default function OrderHistoryPage() {
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const orderData = queryParams.get('orderData');
+    const order = JSON.parse(decodeURIComponent(orderData));
     return (
-      <>
-        <h1>OrderHistoryPage</h1>
-      </>
+      <div>
+        <h1>Order History</h1>
+        {cart && cart.items.length > 0 ? (
+          <ul>
+            {cart.items.map((item, index) => (
+              <li key={index}>
+                <div>
+                  <h3>Item Name: {item.item.name}</h3>
+                  <p>Price: ${item.item.price}</p>
+                  <p>Quantity: {item.qty}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No orders in the history.</p>
+        )}
+      </div>
     );
   }
