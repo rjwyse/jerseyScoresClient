a9618efbabfd820f6099a72972061c80e4a2f3a1
rjwyse <wyse.rob27@gmail.com>
2024-01-31 14:57:08 -0500

    1/31

--- src/utilities/orders-api.js
+++ src/utilities/orders-api.js
@@ -9,7 +9,7 @@ export function getCart() {
 
 // Add an item to the cart
 export function addItemToCart(itemId) {
-  // Just send itemId for best security (no pricing)
+
   return sendRequest(`${BASE_URL}/cart/items/${itemId}`, 'POST');
 }
 
@@ -17,9 +17,9 @@ export function setItemQtyInCart(itemId, newQty) {
   return sendRequest(`${BASE_URL}/cart/qty`, 'PUT', { itemId, newQty });
 }
 
-// Updates the order's (cart's) isPaid property to true
+
 export function checkout() {
-  // Changing data on the server, so make it a POST request
+
   return sendRequest(`${BASE_URL}/cart/checkout`, 'POST');
 }
 
